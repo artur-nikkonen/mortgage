216a4fb45cadb002478e0452ce637d0e8464bc4d
Artur Nikkonen <artur.nikkonen@gmail.com>
2020-07-31 12:15:25 +0300

    Start

---
+++ src/main/java/ru/geekbrains/mortgage/entity/MortgageApplication.java
@@ -0,0 +1,53 @@
+package ru.geekbrains.mortgage.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "mortgage_application")
+public class MortgageApplication {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private ResolutionStatus status;
+
+    public MortgageApplication() {
+    }
+
+    public MortgageApplication(String name, ResolutionStatus status) {
+        this.name = name;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ResolutionStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ResolutionStatus status) {
+        this.status = status;
+    }
+}
